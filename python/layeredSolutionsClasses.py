6373aea7164e010d3c9d9325b973707b8626b08e
mahawkma <woster402@gmail.com>
2018-03-25 17:38:12 -0500

    Changes to be committed: 	modified:   layeredSolutionsClasses.py

--- python/layeredSolutionsClasses.py
+++ python/layeredSolutionsClasses.py
@@ -7,23 +7,27 @@ import datetime
 from bs4 import BeautifulSoup
 from time import mktime
 
+
+#Class to store relevant information about the vulnerbility.
 class vulnObject:
 
     numVulns = 0
 
+    #Init method
     def __init__(self, vulnID, debug):
-        self.vulnID = vulnID
-        self.search_url =''
-        self.cveID = ''
-        self.datePublic = 0.0 
-        self.dateFirstPublished = 0.0
-        self.dateLastUpdated = 0.0
-        self.severityMetric = 0.0
-        self.documentRevision = 0.0
-        self.debug = debug
+        self.vulnID = vulnID #CVE.org Vuln ID
+        self.search_url ='' #search URL that obtianed the info
+        self.cveID = '' #Relevant CVE ID, if any
+        self.datePublic = 0.0 #Date that the vuln went public
+        self.dateFirstPublished = 0.0 #Date vuln was first published in database
+        self.dateLastUpdated = 0.0 #Date vuln we last updated in database
+        self.severityMetric = 0.0 #Severith metric associated with the vuln
+        self.documentRevision = 0.0 #Document revision
+        self.debug = debug #Flag to set debug mode on or off
 
         vulnObject.numVulns +=1
 
+    #Function to convert the Gregorian calander dates to UNIX Epoch dates
     def convertDate2Epoch(self, date, debug):
         self.debug = debug
         days = date[0:2]
@@ -50,6 +54,7 @@ class vulnObject:
 
         return timestamp
 
+    #Functions below are used to set the elements of the object
     def setSearchURL(self, search_url):
         self.search_url = search_url
 
@@ -71,6 +76,7 @@ class vulnObject:
     def setDocumentRevision(self, documentRevision):
         self.documentRevision = documentRevision
 
+    #String function for vuln object
     def __str__(self):
         return('''
             Search URL:             {}
@@ -82,33 +88,37 @@ class vulnObject:
             Severity Metric:        {}
             Document Revision:      {}'''.format(self.search_url, self.vulnID, self.cveID, self.datePublic, self.dateFirstPublished, self.dateLastUpdated, self.severityMetric, self.documentRevision))
 
+#Class to search the database. It is passed three parameters. A true or false debug flag, the product being searched for, and the max number of results to return
 class Search:
 
+    #Function that will search and parse the results from the search
     def searchVuln(self, debug, product, searchMax):
-        vulnList = []
-        urlList = []
-        searchDict = {}
-        payload = {'query': product, 'searchmax': searchMax, 'searchorder': '1'}
-        results = requests.get('https://kb.cert.org/vuls/byid?searchview', params=payload)
-        soup = BeautifulSoup(results.text, 'html.parser')
+        vulnList = [] #List to store parsed URLs from the query
+        urlList = [] #List to store the vulnIDs
+        searchDict = {} #Dict to hold the vuln objects with the key being the vulnID
+        payload = {'query': product, 'searchmax': searchMax, 'searchorder': '1'} #payload dict that is passed to the get request
+        results = requests.get('https://kb.cert.org/vuls/byid?searchview', params=payload) #get request to obtain results 
+        soup = BeautifulSoup(results.text, 'html.parser') #BeutifulSoup is used to parse the reults
 
-        for link in soup.find_all('a'):
+        for link in soup.find_all('a'): #Searches through the results for a paticular pattern to obtain partial urls with vulnIDs
             if re.search('/vuls/id/[0-9]*', str(link)):
                 vulnList.append(str(re.findall('/vuls/id/[0-9]*', str(link))))
 
-        for num in range(0,len(vulnList)):
+        for num in range(0,len(vulnList)): #parses the vulnList for the vulnIDs
             string = re.sub('[^0-9]+','',vulnList[num])
             urlList.append(string)
 
-        for num in range(len(urlList)):
-            vuln = vulnObject(urlList[num], debug)
-            search_url = 'https://kb.cert.org/vuls/id/' + urlList[num]
+        #For each vulnID in the vulnList:
+        for num in range(len(urlList)): 
+            vuln = vulnObject(urlList[num], debug) #obstabtiates the vuln object
+            search_url = 'https://kb.cert.org/vuls/id/' + urlList[num] #search URL used to obtain results
             vuln.setSearchURL(search_url)
             tempList = []
-            vul_results = requests.get(search_url)
-            parsed_results = BeautifulSoup(vul_results.text, 'html.parser')
-            other_info = parsed_results.find(id="other-info")
+            vul_results = requests.get(search_url) #Get request to pull details of the vulnerability
+            parsed_results = BeautifulSoup(vul_results.text, 'html.parser') #Parsing the results with BeautifulSoup
+            other_info = parsed_results.find(id="other-info") #Find the other-info section from the parsed results and creating a reference to them. This section holds in information on the vuln we are interested in
 
+            #Find all the information from the other-info section and place the information we are interested in into the appropriate place in the vulnObject
             for li in other_info.find_all('li'):
                 data1 = li.find_all('span')[0]
                 data2 = li.find_all('span')[1]
@@ -174,6 +184,7 @@ class Search:
             print(str(results[item]))
 
 if __name__ == "__main__":
+    #Parsing the command line for arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', help='turn on script debugging', action='store_true', default=False)
     parser.add_argument('product', type=str)
